3ac5d86c73cad1435a3ae7b108ae08e237100c6b
Oluremi Abraham <olurmeiabraham9@gmail.com>
2023-06-03 20:21:28 +0000

    Tasks done partially

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,19 @@
+$('document').ready(function () {
+  $.get('http://127.0.0.1:5001/api/v1/status/', function(response) {
+    if (response.status === 'OK') {
+      $('div#api_status').addClass('available');
+    } else {
+      $('div#api_status').removeClass('available');
+    }
+  })
+
+  let amenities = {};
+  $('INPUT[type="checkbox"]').change(function () {
+    if ($(this).is(':checked')) {
+      amenities[$(this).attr('data-id')] = $(this).attr('data-name');
+    } else {
+      delete amenities[$(this).attr('data-id')];
+    }
+    $('.amenities H4').html(Object.values(amenities).join(', '));
+  });
+});
